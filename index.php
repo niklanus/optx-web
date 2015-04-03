9819ffdf7249f2b05453320de3d5bd3942a32788
Nik Lanus <nicolaslanus@gmail.com>
2015-04-03 15:42:52 -0300

    Setup nuevo sitio

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--[if IE 8]>    <html lang="en-us" class="ie8"> <![endif]-->
+<!--[if lt IE 9]><!--> <html lang="es" class="ie9plus"> <!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<title>OpTx</title>
+	
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/blueprints.css">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic' rel='stylesheet' type='text/css'>
+
+    <!--[if lt IE 9]>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	<h1>Hello world!</h1>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/retina.min.js"></script>
+	<script src="js/smooth-scroll.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/featherlight.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*$(window).on("load resize",function(){
+	var vw = $(window).width();
+	var vh = $(window).height();
+	$('').height(vh);
+
+	if (vw <= 1023) {
+		$("nav").addClass('nav-mobile');
+		$('.nav-items').addClass('hidden');
+	} else {
+		$("nav").removeClass('nav-mobile');
+	}
+});
+
+$( document ).ready(function() {
+	
+});*/

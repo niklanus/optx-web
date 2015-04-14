150dfae504ebe0325ce0d1f2e9d5531e4ca5c865
Nik Lanus <nicolaslanus@gmail.com>
2015-04-14 19:35:14 -0300

    Fix

---
+++ ftth.php
@@ -0,0 +1,68 @@
+	<?php include_once("includes/header.php") ?>
+
+	<section class="service-section ftth">
+		<div class="jumbotron">
+			<div class="container">
+				<h2>FTTH - Fiber to the Home</h2>
+			</div>
+		</div>
+
+		<div class="container service-text">
+			<p>El FTTH con el modelo autónomo, significa tomar control de la distribución de última milla de las comunicaciones, quedando a nivel interno los servicios de mantenimiento e incorporación de usuarios dentro del urbanismo.</p>
+			<p>Los contratos de servicios se pueden llevar a formato colectivo, quedando en manos de una administración tipo consorcio, que permiten obtener costos más bajos por volumen.</p>
+			<p>Con la infraestructura adecuada en el NOC (Network Operation Center), se podría transportar por la misma fibra a diferentes proveedores de servicios, ya sea de Telefonía, TV o Internet, desde la cabecera hasta el usuario sin tener que dar acceso a los instaladores de las operadoras de servicios al barrio.</p>
+			<p>Se puede contar con un tarifador que emita resumen de consumo telefónico por usuario cada mes.</p>
+			<p>Un gestor de cámaras, DVR, administraría las grabaciones de su sistema de vigilancia y así mismo se puede brindar acceso selectivo a su personal de vigilancia o un proveedor de seguridad externo a través de Internet.</p>
+			<p>Un gestor de control de accesos, permitiría incorporar sistemas de autenticación biométricos para activar barreras y dejar constancia de ingreso/egreso de las personas registradas.</p>
+			<p>Se puede conectar todo sistema que cuente con interfaz IP (Internet Protocol), el estándar más común del mercado.</p>
+
+			<hr>
+
+			<h3 class="title-with-lead">Características</h3>
+			<p class="lead">el producto cuenta con la capacidad de distribuir Telefonía, TV e Internet.</p>
+
+			<p>Internet requiere conectarse por medio de un servicio mayorista de Internet en el extremo de cabecera, servicio que será fraccionado para su distribución, esto resuelve el servicio de Telefonía de forma conjunta. La telefonía será de Telecom, Telefónica, Claro, IPlan o cualquier proveedor que brinde servicios de telefonía IP, podrá contratarse individualmente o en grupo y requerirá un gestor de tarifación, una especie de central telefónica que lleva el detalle de consumo de cada línea.</p>
+			<p>TV requerirá la señal para ser  inyectada en la red, como ejemplo TDA (Televisión Digital Abierta). Esta señal será recibida por medio de un sistema de antenas y decodificadores, los que entregarán múltiples canales a un homologador, éste a su vez lo entregará a un conversor que lo inyectará en la fibra que lo llevará hasta el usuario.</p>
+
+			<hr>
+
+			<h3 class="title-with-lead">Implementación</h3>
+			<p class="lead">nos encargamos tanto del trabajo de campo como del de sistemas.</p>
+			
+			<p>Contamos con los ingenieros, técnicos, obreros y equipamiento necesario para desarrollar todas las etapas de la implementación, obra civil, saneado de ductos y cableados hasta cada pilar del barrio en la primer etapa. Luego en la etapa de sistemas implementamos lo servidores para las diferentes prestaciones. Finalmente la iluminación de la red y las alta de usuarios.</p>
+
+			<hr>
+
+			<h3 class="title-with-lead">Gerenciamiento</h3>
+			<p class="lead">contamos con la capacidad de gestionar su red de telecomunicaciones.</p>
+
+			<p>Mediante un sistema CRM brindamos al usuario un sistema de comunicación eficiente, que nos permite hacer seguimiento de cada caso manteniendo registro de cada atención y solución brindada. En cabecera, será la gestión de proveedores y la emisión del resumen mensual para los usuarios. El alta de servicios, contar con una cuadrilla que resolverá el cableado desde el pilar hasta en interior de la casa del nuevo usuario. Brindamos a los usuarios un sistema en nuestra página web que facilita la gestión de solicitudes de atención a las que respondemos con técnicos de nuestro equipo.</p>
+
+
+			<!--
+			<div class="row service-pics">
+				<div class="col-sm-6">
+					<div class="thumbnail">
+						<img src="img/ftth-2.jpg" alt="" class="img-responsive">
+						<div class="caption">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="thumbnail">
+						<img src="img/ftth-3.jpg" alt="" class="img-responsive">
+						<div class="caption">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+						</div>
+					</div>
+				</div>
+			</div> -->
+			
+		</div>
+
+	</section>
+
+	<?php include_once("includes/footer.php") ?>
+</body>
+</html>
